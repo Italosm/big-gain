d055f7147274ba59e9b5804034f5dcf484bee1b6
Italo Souza Melo <italosm29@gmail.com>
2024-08-17 08:43:38 -0300

    bug: zod db url

--- src/shared/infrastructure/env-config/env.ts
+++ src/shared/infrastructure/env-config/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3000),
   STRIPE_SECRET: z.string(),
   STRIPE_ID_WEBHOOK: z.string(),
