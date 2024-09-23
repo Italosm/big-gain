732c1e795d74d6b4f2c775da767a16de758ad174
Italo Souza Melo <italosm29@gmail.com>
2024-09-23 17:09:01 -0300

    period end checkout

--- src/shared/services/CheckoutSessionService.ts
+++ src/shared/services/CheckoutSessionService.ts
@@ -2,6 +2,7 @@ import Stripe from 'stripe';
 import ApplicationError from '../errors/application-error';
 import { prismaService } from '../infrastructure/database/prisma/prisma.service';
 import { NotFoundError } from '../application/errors/not-found-error';
+import { stripe } from '../utils/stripe';
 
 class CheckoutSessionService {
   public async execute(event: { data: { object: Stripe.Checkout.Session } }) {
@@ -26,12 +27,21 @@ class CheckoutSessionService {
       throw new NotFoundError('User not found.');
     }
 
+    const subscription = await stripe.subscriptions.retrieve(subscription_id);
+
+    if (!subscription) {
+      throw new NotFoundError('Subscription not found.');
+    }
+
+    const current_period_end = subscription.current_period_end;
+
     const updatedStripeSubscription =
       await prismaService.stripeSubscription.update({
         where: { user_id: userExists.id },
         data: {
           customer_id,
           subscription_id,
+          current_period_end: new Date(current_period_end * 1000),
         },
       });
 
