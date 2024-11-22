e55cb7a4cd7d07ea86426717e1b1dbcd3bcd8a75
Italo Souza Melo <italosm29@gmail.com>
2024-11-21 22:28:05 -0300

    rollback

--- src/shared/infrastructure/http/routes/telegram.routes.ts
+++ src/shared/infrastructure/http/routes/telegram.routes.ts
@@ -13,65 +13,62 @@ export function initializeBot() {
   });
 
   bot.on(message('text'), async ctx => {
-    console.log('Chat ID:', ctx.chat.id);
+    const code = ctx.message.text;
+    const chatId = ctx.chat?.id;
+    console.log(chatId);
+
+    try {
+      const tokenEntry = await prismaService.telegramToken.findFirst({
+        where: {
+          token: code,
+          expired_in: {
+            gt: new Date(), // Verifica se o token ainda não expirou
+          },
+        },
+        include: {
+          user: true,
+        },
+      });
+
+      if (tokenEntry) {
+        await ctx.reply(
+          'Código validado! Agora, por favor, compartilhe seu número de telefone:',
+          {
+            reply_markup: {
+              one_time_keyboard: true,
+              keyboard: [
+                [
+                  {
+                    text: 'Enviar meu número de telefone',
+                    request_contact: true,
+                  },
+                ],
+              ],
+            },
+          },
+        );
+
+        // Armazena temporariamente o user_id e chatId para futura associação com o telefone
+        await prismaService.userTelegram.upsert({
+          where: { user_id: tokenEntry.user_id },
+          create: {
+            user_id: tokenEntry.user_id,
+            chat_id: chatId,
+          },
+          update: { chat_id: chatId },
+        });
+      } else {
+        await ctx.reply(
+          'Código inválido ou expirado. Por favor, tente novamente.',
+        );
+      }
+    } catch (error) {
+      console.error('Erro ao validar o código:', error.message);
+      await ctx.reply(
+        'Ocorreu um erro ao validar seu código. Tente novamente.',
+      );
+    }
   });
-
-  // bot.on(message('text'), async ctx => {
-  //   const code = ctx.message.text;
-  //   const chatId = ctx.chat?.id;
-
-  //   try {
-  //     const tokenEntry = await prismaService.telegramToken.findFirst({
-  //       where: {
-  //         token: code,
-  //         expired_in: {
-  //           gt: new Date(), // Verifica se o token ainda não expirou
-  //         },
-  //       },
-  //       include: {
-  //         user: true,
-  //       },
-  //     });
-
-  //     if (tokenEntry) {
-  //       await ctx.reply(
-  //         'Código validado! Agora, por favor, compartilhe seu número de telefone:',
-  //         {
-  //           reply_markup: {
-  //             one_time_keyboard: true,
-  //             keyboard: [
-  //               [
-  //                 {
-  //                   text: 'Enviar meu número de telefone',
-  //                   request_contact: true,
-  //                 },
-  //               ],
-  //             ],
-  //           },
-  //         },
-  //       );
-
-  //       // Armazena temporariamente o user_id e chatId para futura associação com o telefone
-  //       await prismaService.userTelegram.upsert({
-  //         where: { user_id: tokenEntry.user_id },
-  //         create: {
-  //           user_id: tokenEntry.user_id,
-  //           chat_id: chatId,
-  //         },
-  //         update: { chat_id: chatId },
-  //       });
-  //     } else {
-  //       await ctx.reply(
-  //         'Código inválido ou expirado. Por favor, tente novamente.',
-  //       );
-  //     }
-  //   } catch (error) {
-  //     console.error('Erro ao validar o código:', error.message);
-  //     await ctx.reply(
-  //       'Ocorreu um erro ao validar seu código. Tente novamente.',
-  //     );
-  //   }
-  // });
 
   // Recebe o contato do usuário após ele compartilhar o número
   bot.on(message('contact'), async ctx => {
